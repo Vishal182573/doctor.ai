e4797fb22356e3ee732bd9dc90ade98034ade899
Vishal Sharma <sharmavs9205@gmail.com>
2024-05-13 03:25:07 +0530

    all directories setup , shadcn setup ,basic structure is ready

---
+++ doctor.ai/src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      This is login page
+    </div>
+  )
+}
+
+export default page
---
+++ doctor.ai/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      This is home page
+    </div>
+  );
+}
